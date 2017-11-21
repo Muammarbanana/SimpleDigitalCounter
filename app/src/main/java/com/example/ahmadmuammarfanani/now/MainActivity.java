bf67d187fc8a93da2cfe7dd06ae936d6aab5a7af
Muammar Fanani <muammarfanani2@gmail.com>
2017-11-21 10:56:17 +0700

    Penambahan Fitur Getar

--- app/src/main/java/com/example/ahmadmuammarfanani/now/MainActivity.java
+++ app/src/main/java/com/example/ahmadmuammarfanani/now/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.ahmadmuammarfanani.now;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,14 +21,25 @@ public class MainActivity extends AppCompatActivity {
         final Button b = findViewById(R.id.button);
         final TextView a = findViewById(R.id.text1);
         final Button reset = findViewById(R.id.button2);
+        final Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        final CheckBox geter = findViewById(R.id.cbGetar);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String d = (String) a.getText();
-                int c = Integer.parseInt(d);
-                c++;
-                String e = String.valueOf(c);
-                a.setText(e);
+                if(geter.isChecked()){
+                    v.vibrate(100);
+                    String d = (String) a.getText();
+                    int c = Integer.parseInt(d);
+                    c++;
+                    String e = String.valueOf(c);
+                    a.setText(e);
+                }else{
+                    String d = (String) a.getText();
+                    int c = Integer.parseInt(d);
+                    c++;
+                    String e = String.valueOf(c);
+                    a.setText(e);
+                }
             }
         });
         reset.setOnClickListener(new View.OnClickListener(){
